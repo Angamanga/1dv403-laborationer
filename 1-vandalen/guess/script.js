eabe115152f99ebb3fe4dda041875fa6f62111d6
Angamanga <Angamanga@annas-mbp.lan>
2014-11-05 15:34:44 +0100

    Börjat på Lab1

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,34 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret =  Math.floor( Math.random() * (100-1)+1) + 1;
+	var count=1;
+	 // Detta tal behöver bytas ut mot ett slumpat tal.
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var guess = function(number){
+		
+		var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
 		// Plats för förändring.
 
+		while(number!==secret)
+			{count++;
 
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		if(number<secret)
+		{
+			return [false, "Det hemliga talet är högre!"];
+		}
+		else if(number>secret){
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		else{
+			return [false, "Talet är utanför intervallet 0 - 100"]
+		}
+}
+return [true, "Grattis du vann! Det hemliga talet var "+secret+ " och du behövde "+count +" gissningar för att hitta det."];
+			
+
 	};
 	
 	// ------------------------------------------------------------------------------
