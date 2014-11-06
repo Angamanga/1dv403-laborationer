4e8ea9cf21e63fed699446dfe5e0dcb9cc4e78cd
Angamanga <Angamanga@annas-mbp.lan>
2014-11-06 10:06:31 +0100

    Uppgift 1 - Gissa det hemliga talet klar

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,32 +2,32 @@
 
 window.onload = function(){ 
 	
-	var secret =  Math.floor( Math.random() * (100-1)+1) + 1;
-	var count=0;
-	 // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var secret =  Math.floor( Math.random() * (100-1)+1) + 1; // Slumpat tal mellan 1-100.
+	var count=0; //variabel för att räkna antalet gissningar. Deklareras utanför guessfunktionen för att inte nollställas varje gång en gissning görs.
+	 
+
 	// I denna funktion ska du skriva koden för att hantera "spelet"
-		
-		var guess = function(number){
+
+	var guess = function(number){
 		//console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		//console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 		// Plats för förändring.
-		count++;
 		
-		if(number<secret)
-		{
+		count++; //Räknare för antalet gissningar.
+		
+		//Kontrollerar gissningen mot secret number och
+		if(number==secret){ 
+			return [true, "Grattis du vann! Det hemliga talet var " +secret +" och du behövde " +count +" gissningar för att hitta det."];
+		}
+		else if(number<secret && number>0){
 			return [false, "Det hemliga talet är högre!"];
 		}
-		else if(number>secret){
+		else if(number>secret&&number<101){
 			return [false, "Det hemliga talet är lägre!"];
 		}
-		else if(number<0||number>100){
+		else{
 			return [false, "Talet är utanför intervallet 0 - 100"];
 		}
-		else
-			{return [true, "Grattis du vann! Det hemliga talet var " +secret +" och du behövde " +count +" gissningar för att hitta det."];
-			}
-
 	};
 	
 	// ------------------------------------------------------------------------------
@@ -49,6 +49,6 @@ window.onload = function(){
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
 			submit.disabled = true;
 		}
-	
+
 	});
 };
