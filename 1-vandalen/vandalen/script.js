6d82ffbcfeba4bc5c92e5b8866b0cd043210e2b1
Angamanga <Angamanga@annas-mbp.lan>
2014-11-11 23:16:09 +0100

    alla varden raknas ut

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,20 +1,33 @@
 "use strict";
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}]; //input array med objekt
 
+
+
+var makePerson = function(persArr){
+
+//funktion for att sortera arrayen
 function sortByKey(array, key) {
     return array.sort(function(a, b) {
         var x = a[key]; var y = b[key];
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });}
 
-var makePerson = function(persArr){
+
 var sortedPersArr=sortByKey(data,'age');//array med objekten sorterade efter age
 var minAge = sortedPersArr[0].age;//lagsta alder
 var maxAge = sortedPersArr[sortedPersArr.length-1].age;//hogsta alder
-var sum = sortedPersArr.reduce(function(a,b,i){return a+b;})//FORTSATT HAR, FUNKAR EJ!
-var average =sum/(persArr.length);
-var names;
-console.log(sortedPersArr);
+
+
+var sum = sortedPersArr.reduce(function (a, b) {
+  return {age: a.age + b.age}; // raknar ut summan av aldrarna i arrayen
+})
+
+var average =sum.age/(sortedPersArr.length); //raknar ut medelvardet av aldrarna
+var sortedNames = sortByKey(persArr, name);//sorterar objekten map namn
+var listNames = sortedNames.reduce(function (a, b) {return (a.name || a) + ", " + b.name});//tva satt att skriva ut namnen i en string
+var test = sortedNames.map(function(elem){
+    return elem.name;
+}).join(", ");
 }
 
 makePerson(data);
@@ -30,3 +43,8 @@ return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Le
 var result = makePerson(data);
 
 console.log(result);
+
+
+function addLast(prev, current) {
+        return prev + current;
+    }
