211e6b275f1411088822e180cfbc56b95a6b16c3
Angamanga <Angamanga@annas-mbp.lan>
2014-11-11 13:43:05 +0100

    lab 1.3 klar, bara fix for att snygga till kvar

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -4,42 +4,46 @@ window.onload = function(){
 
 	
 	var birthday = function(date){
-		//kontrollerar att inmatat datum ar i ratt format.
-	if(date.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/)){
 	var now=Date.now(); 
 	var today = new Date(now);//datumobjekt for idag
 	var todayYear=today.getFullYear(); //tar fram aktuellt ar
-	console.log(todayYear);
 	var birthday = new Date(date);//datumobjekt for fodelsedagen
 	var a; //hjalpvariabel for att kunna skapa datumobjekt for nasta fodelsedag
+	var nB;  //andrar artalet sa att det stammer overens med nar nasta fodelsedag intraffar
+	var nextBirthday; //datumobjekt for nasta fodelsedag
+	var msToBirthday;//ms till nasta fodelsedag
+	var days;//dagar till nasta fodelsedag
 
-//if-sats for att kontrollera om nasta fodelsedag intraffar i ar eller nasta ar
-	if(birthday.getMonth()>today.getMonth()){
+	//kontrollerar att inmatat datum ar i ratt format.
+	if(date.match(/^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/)){
+	
+	
+			//if-sats for att kontrollera om nasta fodelsedag intraffar i ar eller nasta ar
+		if(birthday.getMonth()>today.getMonth()){
 			a=todayYear;
-	}
-	else if(birthday.getMonth()<today.getMonth()){
-		a=todayYear+1;
-	}
-	else{
-		if(birthday.getDate()>=today.getDate()){
-			a=todayYear;	
-				}
-		else if(birthday.getDate()<today.getDate()){
+		}
+		else if(birthday.getMonth()<today.getMonth()){
 			a=todayYear+1;
+		}
+		else{
+			if(birthday.getDate()>=today.getDate()){
+				a=todayYear;	
+			}
+			else if(birthday.getDate()<today.getDate()){
+				a=todayYear+1;
 			}
 		}
+
+		nB=birthday.setFullYear(a);
+		nextBirthday = new Date(nB);
+		msToBirthday= nextBirthday.getTime() - today.getTime(); 
+		days=Math.ceil(msToBirthday/86400000);
 	
-	var nB = birthday.setFullYear(a); //andrar artalet sa att det stammer overens med nar nasta fodelsedag intraffar
-	var nextBirthday = new Date(nB); //datumobjekt for nasta fodelsedag
-	var msToBirthday = nextBirthday.getTime() - today.getTime(); //ms till nasta fodelsedag
-	var days = Math.floor(Math.abs(msToBirthday/86400000));//dagar till nasta fodelsedag
-	console.log(days);
-	console.log(parseInt(days)+1);
-	return parseInt(days)+1;//returnerar dagar kvar till nasta fodelsedag
-}
+		return days;//returnerar dagar kvar till nasta fodelsedag
+	}
 	else{ 
 		throw new Error("Du har matat in födelsedagen i fel format. Använd formatet 'YYYY-MM-DD'");//om felaktigt format matats in kastas ett undantag.
-	}
+		}
 	};
 	// ------------------------------------------------------------------------------
 
