360add186a1e1eb43e232005db4ab8a141bfc657
Angamanga <Angamanga@annas-mbp.lan>
2014-11-11 15:56:09 +0100

    inte fardig, hamta pa dagis

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,10 +1,18 @@
 "use strict";
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}]; //input array med objekt
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });}
+
 var makePerson = function(persArr){
-var sortedPersArr=persArr.sort(age);
-var maxAge;
-var minAge;
-var averageAge;
+var sortedPersArr=sortByKey(data,'age');//array med objekten sorterade efter age
+var minAge = sortedPersArr[0].age;//lagsta alder
+var maxAge = sortedPersArr[sortedPersArr.length-1].age;//hogsta alder
+var sum = sortedPersArr.reduce(function(a,b,i){return a+b;})//FORTSATT HAR, FUNKAR EJ!
+var average =sum/(persArr.length);
 var names;
 console.log(sortedPersArr);
 }
@@ -13,7 +21,12 @@ makePerson(data);
 console.log(data);
 
 
-data.sort(function(a, b){
- return a.age-b.age
-});
+var makePerson = function(persArr) { var result = {};
 
+// Lös uppgiften
+
+return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);
