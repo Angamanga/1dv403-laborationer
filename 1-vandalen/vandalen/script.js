c546e33a96bac5c222bc9d2d63f782abd995f94a
Angamanga <Angamanga@annas-mbp.lan>
2014-11-12 13:50:41 +0100

    1.4 Funkar men behover snygga till och fixa felhantering

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,48 +1,49 @@
 "use strict";
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}]; //input array med objekt
 
-
-
 var makePerson = function(persArr){
 
+var sortedPersArr;
+var minAge;
+var maxAge;
+var sum;
+var average;
+var sortedNames;
+var listNames;
+var names;
+
 //funktion for att sortera arrayen
-function sortByKey(array, key) {
+function sortByKey(array, key) {   
     return array.sort(function(a, b) {
         var x = a[key]; var y = b[key];
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });}
+sortedPersArr=sortByKey(data,'age');			//array med objekten sorterade efter age
+minAge = sortedPersArr[0].age;						//lagsta alder
+maxAge = sortedPersArr[sortedPersArr.length-1].age;	//hogsta alder
+sum = sortedPersArr.reduce(function (a, b) {
+  return {age: a.age + b.age}; 						// raknar ut summan av aldrarna i arrayen
+})
+average =Math.ceil(sum.age/(sortedPersArr.length)); 			//raknar ut medelvardet av aldrarna
+			
 
-
-var sortedPersArr=sortByKey(data,'age');//array med objekten sorterade efter age
-var minAge = sortedPersArr[0].age;//lagsta alder
-var maxAge = sortedPersArr[sortedPersArr.length-1].age;//hogsta alder
-
-
-var sum = sortedPersArr.reduce(function (a, b) {
-  return {age: a.age + b.age}; // raknar ut summan av aldrarna i arrayen
+listNames = persArr.map(function(elem){		//laser ut namnen och returnerar dom som en array
+    return elem.name;
 })
 
-var average =sum.age/(sortedPersArr.length); //raknar ut medelvardet av aldrarna
-var sortedNames = sortByKey(persArr, name);//sorterar objekten map namn
-var listNames = sortedNames.reduce(function (a, b) {return (a.name || a) + ", " + b.name});//tva satt att skriva ut namnen i en string
-var test = sortedNames.map(function(elem){
-    return elem.name;
-}).join(", ");
+names=((listNames.sort(function(a, b) {			//sorterar arrayen och gör om den till en sträng separerade med ", "
+return a.localeCompare(b);
+})).join(', ')).toString(); 
+
+return {minAge:minAge, maxAge:maxAge, averageAge:average, names:names};
+
+
+
 }
 
-makePerson(data);
-console.log(data);
-
-
-var makePerson = function(persArr) { var result = {};
-
-// Lös uppgiften
-
-return result; } var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
-
 var result = makePerson(data);
-
 console.log(result);
+
 
 
 function addLast(prev, current) {
