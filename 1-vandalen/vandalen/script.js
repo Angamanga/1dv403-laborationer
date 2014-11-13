a622581eedff0d0c6d5829b681f89b98484e8540
Angamanga <Angamanga@annas-mbp.lan>
2014-11-13 20:44:43 +0100

    lab 1 fardig for redovisning

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -13,43 +13,46 @@ var average;
 
 function Ages(input)
 {
-	
-		arrayAge=input.map(function(input){				//laser ut namnen och returnerar dom som en ny array
-			if(typeof(input.age) !== "number")	
-			{
-				throw new Error("den inlasta arrayen innehaller inga egenskaper 'age' av typen number");
-			}
-			return input.age;
+			arrayAge=input.map(function(input){				//laser ut namnen och returnerar dom som en ny array
+			// if(typeof(input.age) !== "number")	
+			// {
+			// 	throw new Error("den inlasta arrayen innehaller inga egenskaper 'age' av typen number");
+			// }
+				return input.age;
 			}).sort();
 		
 		minAge = arrayAge[0];						//lagsta alder
 		maxAge = arrayAge[arrayAge.length-1];		//hogsta alder
-		average = arrayAge.reduce(function(a, b) 	// raknar ut summan av aldrarna i arrayen
+		average = arrayAge.reduce(function(a, b) 	//raknar ut summan av aldrarna i arrayen
 		{
 			return a + b;
 		});										
 		average = Math.ceil(average/(arrayAge.length)); //raknar ut medelvardet av aldrarna
 }	
+
+var ages = Ages(persArr);
+
 var arrayName;
+
 function Names(input)
 {
 	
-		arrayName = persArr.map(function(input){
-		if(typeof(input.name) !== "string")
-		{		throw new Error("den inlasta arrayen innehaller inga egenskaper'name' av typen string");
+			arrayName = persArr.map(function(input){    //laser ut namnen och returnerar dom som en ny array
+			// if(typeof(input.name) !== "string")
+			// {		
+			//  throw new Error("den inlasta arrayen innehaller inga egenskaper'name' av typen string");
     			
-    	}
-    	return input.name;
-					});
+	 		 //} 
+	 		    return input.name;
+			});
 		
-		arrayName.sort(function(a, b) {			//sorterar arrayen
+			arrayName.sort(function(a, b) {			//sorterar arrayen
 					return a.localeCompare(b);
 				});
 
-		return arrayName.join(', ');						//gor om arrayen till en strang separerade med ", "			
+		return arrayName.join(', ');				//returnerar en strang separerade med ", "			
 	}
 	
-var ages = Ages(persArr);
 var names = Names(persArr);
 
 return {minAge:minAge, maxAge:maxAge, averageAge:average, names:names};
