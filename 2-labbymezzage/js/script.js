3373952e54d36bbabd10ffd8bdfced30fd510eec
angamanga <anna.iosif@me.com>
2014-11-27 14:11:26 +0000

    return och shift return funkar

--- 2-labbymezzage/js/script.js
+++ 2-labbymezzage/js/script.js
@@ -1,69 +1,95 @@
+"use strict";
 
-"use strict"
-
-window.onload = function(){
-
-var MessageBoard = {
-    messages:[],
-    init:function(e){
-var message = new Message();
-    },
-writeMessage: function (){
-    console.log(MessageBoard.message[0].toString());
-    
-	}
-};
-MessageBoard.init();	
-function writeMessage(input){
-     var messContainer = document.createElement("div");         //  <div class ="messages">
-    messContainer.className = "messages";
-     var container = document.querySelector(".container");
-    container.appendChild(messContainer);
-    var text = document.createTextNode(input);
-    messContainer.appendChild(text);
-    var img1 =document.createElement("img");
-    img1.className ="showDate";
-    img1.src="pics/14.png";
-    img1.alt="visa tid";
-    var img2 =document.createElement("img");
-    img2.className ="delete";
-    img2.src="pics/12.png";
-    img2.alt="radera meddelande";
-    messContainer.appendChild(img1);
-    messContainer.appendChild(img2);
-    
-    // container.appendChild("messContainer");
-    // messContainer.appendChild(image);
-    // messContainer.appendChild(image);
-    // messContainer.appendChild(text);
-
-//     <img src="pics/14.png" alt="tid" class="showDate"></img>
-  //              <img src="pics/12.png" alt="radera meddelande" class="delete"></img>
-    //         </div>    
-}	
-
+window.onload = function() {
+  var MessageBoard = {
+        messages: [],
+        init: function() {
+            var message = new Message();
+            },
+        removeMessage: function(i){
+                    MessageBoard.messages.splice(i,1)
+            },
+        alertDate: function(i){
+            var p = 
+            alert(p);
+        },
+        renderMessage: function(messageID){
+            var container = document.querySelector(".container");
+            var messContainer = document.createElement("div"); 
+            messContainer.className = "messages";
+            container.insertBefore(messContainer);
+            var textP = document.createElement("p");
+            textP.setAttribute("class","text");
+            var timeP = document.createElement("p");
+            timeP.setAttribute("class","time");
+            var text = document.createTextNode(MessageBoard.messages[messageID].getText());
+            var time = document.createTextNode(MessageBoard.messages[messageID].getDate().getHours() +":"+ MessageBoard.messages[messageID].getDate().getMinutes() + ":"+MessageBoard.messages[messageID].getDate().getSeconds());
+            var linkDelete=document.createElement("a");
+            linkDelete.setAttribute("src", "#");
+            var img2 = document.createElement("img");
+            img2.setAttribute("class","submitDelete");
+            img2.setAttribute("src","pics/12.png");
+            img2.setAttribute("alt","radera meddelande");
+            linkDelete.appendChild(img2);
+             messContainer.appendChild(linkDelete);
+            img2.onclick = function(messageID){
+                console.log(MessageBoard.messages.length);
+                MessageBoard.removeMessage(messageID);
+                MessageBoard.renderMessages();
+                console.log(MessageBoard.messages.length);
+                p.innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
+            }
+            messContainer.appendChild(img2);
+            messContainer.appendChild(textP);
+            textP.appendChild(text);
+            messContainer.appendChild(timeP);
+            timeP.appendChild(time);
+            
+             var img1 = document.createElement("img");
+            img1.setAttribute("class","showDate");
+            img1.setAttribute("src","pics/14.png");
+            img1.setAttribute("alt","visa tid");
+            messContainer.appendChild(img1);
+            var alertbox=MessageBoard.messages[messageID].getDate();
+            img1.onclick = function(messageID){
+            alert("Inlagget skapades den: " + alertbox);
+        }}
+            ,
+            renderMessages: function(){
+            document.querySelector(".container").innerHTML="";
+            for(var i=0;i<MessageBoard.messages.length;++i){
+                MessageBoard.renderMessage(i);
+            }
+  }};
+  
+  MessageBoard.init();
+  
 
 var messageInput = document.querySelector(".messageInput");
 var submitMessage = document.querySelector(".submitMessage");
 var messages = document.querySelector(".messages");
-var p = document.querySelector(".counter")
+var p = document.querySelector(".counter");
 
-submitMessage.addEventListener("click", function(e){
-var txt = messageInput.value;
-var dte = new Date();
-var msg = new Message(txt,dte);
-MessageBoard.messages.push(msg);
-p.innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
-
-writeMessage(txt);
-
-
-
+function submit(){
+        var txt = messageInput.value;
+        var dte = new Date();
+        var msg = new Message(txt, dte);
+        MessageBoard.messages.push(msg);
+        p.innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
+        MessageBoard.renderMessages();
+        console.log(MessageBoard.messages);}
+        
+ submitMessage.addEventListener("click", function(e) {
+       submit();
 });
 
-var showDate = document.querySelector(".showDate");
-showDate.addEventListener("click",function(e){
-    alert("Inlagget skapades den" + this.getDate());            //kolla upp!
-    }
-);
+ messageInput.onkeypress = function(e){
+     
+       if(e.shiftKey&&e.keyCode==13){
+           console.log("japp");
+       }
+       else if(e.keyCode==13&&e.shiftKey){
+         submit();
+       }
+}
 };
